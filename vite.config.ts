1be4e4cb6d7b7749b1462d325da9f61a69694a36
Erengisle <96127212+Erengisle@users.noreply.github.com>
2026-04-17 21:07:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/historiskabegrepp_kallakriget/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+})
